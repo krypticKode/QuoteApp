b66a18d5a15caa180afcf76d44ce19e594ef9a5b
dac101 <dacor101@gmail.com>
2015-08-11 01:31:19 -0500

    testing

---
+++ app/src/androidTest/java/kode/kryptic/com/superquotes/ApplicationTest.java
@@ -0,0 +1,13 @@
+package kode.kryptic.com.superquotes;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
