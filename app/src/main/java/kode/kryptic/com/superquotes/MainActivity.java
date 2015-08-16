e587fc9908a4e52b29108cff131978f222d4a5c3
dac101 <dacor101@gmail.com>
2015-08-16 14:04:00 -0500

    Add Navigation Draw

--- app/src/main/java/kode/kryptic/com/superquotes/MainActivity.java
+++ app/src/main/java/kode/kryptic/com/superquotes/MainActivity.java
@@ -1,17 +1,36 @@
 package kode.kryptic.com.superquotes;
 
-import android.support.v7.app.ActionBarActivity;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends ActionBarActivity {
+import butterknife.Bind;
+import kode.kryptic.com.superquotes.activity.FragmentDrawer;
+
+public class MainActivity extends AppCompatActivity implements FragmentDrawer.FragmentDrawerListener  {
+    @Bind(R.id.toolbar) Toolbar mToolbar;
+    private FragmentDrawer drawerFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        drawerFragment = (FragmentDrawer)
+                getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
+        drawerFragment.setUp(R.id.fragment_navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout), mToolbar);
+        drawerFragment.setDrawerListener(this);
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -33,5 +52,10 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onDrawerItemSelected(View view, int position) {
+
     }
 }
